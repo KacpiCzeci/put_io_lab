aac2e30f6f0b67aa038a1148ec276ef7c699f2b2
KacpiCzeci <k.trzeciak97@gmail.com>
2021-01-17 22:36:44 +0100

    Testy Akceptacyjne

---
+++ 10-AT/accounts-at/src/test/java/put/selenium/linear/AccountsLinearScriptAT.java
@@ -0,0 +1,54 @@
+package put.selenium.linear;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import put.selenium.utils.ScreenshotAndQuitOnFailureRule;
+
+import static org.junit.Assert.*;
+
+
+public class AccountsLinearScriptAT {
+
+    private WebDriver driver;
+
+    @Rule
+    public ScreenshotAndQuitOnFailureRule screenshotOnFailureAndWebDriverQuitRule =
+            new ScreenshotAndQuitOnFailureRule();
+    private StringBuffer verificationErrors = new StringBuffer();
+
+
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        this.driver = new ChromeDriver();
+        screenshotOnFailureAndWebDriverQuitRule.setWebDriver(driver);
+        driver.get("http://localhost:8080/accounts/controller?action=db_reset");
+    }
+
+    @Test
+    public void successfulUserRegistration() throws Exception {
+        driver.get("http://localhost:8080/accounts/controller");
+        driver.findElement(By.linkText("Register")).click();
+        driver.findElement(By.name("username")).clear();
+        driver.findElement(By.name("username")).sendKeys("JanKow");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("JanK123!");
+        driver.findElement(By.name("repeat_password")).clear();
+        driver.findElement(By.name("repeat_password")).sendKeys("JanK123!");
+        driver.findElement(By.name("name")).clear();
+        driver.findElement(By.name("name")).sendKeys("Jan Kowalski");
+        driver.findElement(By.name("addressData")).clear();
+        driver.findElement(By.name("addressData")).sendKeys("Główna 1, Poznań");
+        driver.findElement(By.name("submit")).click();
+        try {
+            assertEquals("Login", driver.findElement(By.xpath("//h3")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+
+}
